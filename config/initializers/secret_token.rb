71de74fc9f3d9a63ac390107f3f61786514c31d8
Danielle Charles <arridspray@yahoo.com>
2012-03-14 16:31:03 -0400

    new age commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Age::Application.config.secret_token = '70244220ed29912de7fb06058c0796ac483acf883f0fd989452605c71e054ea34d25ed01be0586489d038727956679a0a86361c0213db14e5564cc6779676bf6'
